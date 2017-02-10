5a7724a9a8773cf1ddb5b613e82770699a375725
Kyle <kyle.boorky@gmail.com>
2017-02-10 15:56:49 -0500

    adding http problem

---
+++ error/http-problem.go
@@ -0,0 +1,19 @@
+package error
+
+import "fmt"
+
+// HTTPProblem should be used as the response in case of an error during an HTTP request.
+// It implements the https://datatracker.ietf.org/doc/rfc7807 spec with an addition code
+// field which is meant to be machine readable and give clients enough information to handle the error appropriately.
+type HTTPProblem struct {
+	Type     string `json:"type,omitempty"`
+	Title    string `json:"title,omitempty"`
+	Status   int    `json:"status,omitempty"`
+	Detail   string `json:"detail,omitempty"`
+	Instance string `json:"instance,omitempty"`
+	Code     string `json:"code,omitempty"`
+}
+
+func (h HTTPProblem) Error() string {
+	return fmt.Sprintf("HTTPProblem: [%s] - %s - %s", h.Code, h.Title, h.Detail)
+}
